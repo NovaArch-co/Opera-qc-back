baa75cda45a119651a681fe0a889778a62047f33
moein9gh <moein9gh@gmail.com>
2025-03-11 17:18:00 +0330

    remove bullmq

--- src/cron/redis.ts
+++
@@ -1,7 +0,0 @@
-import {RedisOptions} from "ioredis";
-import {env} from "@/common/utils/envConfig";
-
-export const redisConfig: RedisOptions = {
-    host: env.REDIS_HOST,
-    port: env.REDIS_PORT,
-};
