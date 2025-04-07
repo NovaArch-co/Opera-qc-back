a76758e9eda33d0a006cee38759e22f2fd767e2d
GC-Kalateh <ali.kalate89@hotmail.com>
2025-04-07 09:15:18 +0330

    handle cors

--- src/server.ts
+++ src/server.ts
@@ -21,7 +21,6 @@ const app: Express = express();
 console.log("Swagger URL:", env.SWAGGER_URL);
 
 // Set the application to trust the reverse proxy
-app.use(cors());
 app.set("trust proxy", true);
 
 // Setting up authentication handler
@@ -31,7 +30,13 @@ passport.use(passportConfig);
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+// Configure CORS to allow all origins
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 app.use(rateLimiter);
 
 // Request logging
